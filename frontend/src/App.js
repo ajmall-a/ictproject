3dde59d0c813c60bf11bb6a1c9ebea2ee63f28b0
thoufeekan <thoufeekthoufeek599@gmail.com>
2024-04-29 19:50:44 +0530

    homepage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,12 @@
+import React from 'react'
+import HomePage from './components/HomePage'
 
-import './App.css';
-import { Routes,Route} from 'react-router-dom';
-import Main from './components/Main';
-import Home from './components/Home';
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-import Mentordash from './components/Mentordash';
-import { AddMentorForm } from './components/AddMentorForm';
-import AdminDashboard from './components/AdminDashboard';
-
-
-
-function App() {
+const App = () => {
   return (
     <div>
-      <Routes>
-      <Route path={'/'} element={<Login/>}/>
-      {/* <Route path={'/home'} element={<Main child={<Home/>}/>}/>
-      <Route path={'/navbar'} element={<Main child={<Navbar/>}/>}/> */}
-      <Route path={'/admindashboard'} element={<Main child={<AdminDashboard/>}/>}/>
-      <Route path={'/mentorform'} element={<Main child={<AddMentorForm/>}/>}/>
-      
-      {/* <Route path={'/mentordash'} element={<Main child={<Mentordash/>}/>}/> */}
-     
-      
-      </Routes>
-      
+      <HomePage/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
